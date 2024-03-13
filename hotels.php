bfba09c3b6f3373084a2b3fa1523575515e52629
flo1a <105285535+flo1a@users.noreply.github.com>
2024-03-13 13:19:28 +0100

    1

--- hotels.php
+++ hotels.php
@@ -1,9 +1,11 @@
 <?php
 
 // Load the HTML template
+$htmlMainTemplate = file_get_contents('main.html');
 $htmlTemplate = file_get_contents('hotels.html');
 
-$htmlOut = ''; // Initialize the output variable
+$htmlOut = '';
+$htmlOutFin = ''; // Initialize the output variable
 
 // Define data for hotels
 $hotels = [
@@ -12,8 +14,6 @@ $hotels = [
     ['name' => 'Hotel C', 'description' => 'Description of Hotel C...'],
 ];
 
-$i = 0;
-
 // Loop through hotels and replace placeholders
 foreach ($hotels as $hotel) {
     // Replace placeholders with actual data
@@ -21,20 +21,16 @@ foreach ($hotels as $hotel) {
     $html = str_replace("{hotel_name}", $hotel['name'], $html);
     $html = str_replace("{hotel_description}", $hotel['description'], $html);
 
-    if ($i > 0) {
-        $html = str_replace("{headline}", '', $html);
-    }
-
-    $html = str_replace("{headline}", 'Hotels: ', $html);
-
     // Append the modified HTML for each hotel to the output variable
     $htmlOut .= $html;
-
-    $i++;
 }
+$html2 = $htmlMainTemplate;
+$html2 = str_replace("{headline}", "Hotels: ", $html2);
+$html2 = str_replace("{hotels}", $htmlOut, $html2);
+$htmlOutFin = $html2;
 
 
 
 // Display the final HTML output
-echo $htmlOut;
+echo $htmlOutFin;
 ?>
